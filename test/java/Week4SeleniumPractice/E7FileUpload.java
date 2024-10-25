5aef89aeae35045460881e84e9ee14910160f185
Sorahon84 <shohkuch@gmail.com>
2024-10-25 14:58:49 -0500

    added new file

--- test/java/Week4SeleniumPractice/E7FileUpload.java
+++ test/java/Week4SeleniumPractice/E7FileUpload.java
@@ -1,9 +1,18 @@
 package Week4SeleniumPractice;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class E7FileUpload {
     public static void main(String[] args) {
 
-
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://syntaxprojects.com/upload-image.php");
+       WebElement fileButton = driver.findElement(By.id("image-file"));
+       fileButton.sendKeys("/Users/sora/IdeaProjects/SeleniumBasics/screenshot/image.png");
 
 
 
